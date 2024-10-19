d02bfdb240eb1357ed5ddd167dd56c9bcbf49ce1
notAnii <animazingtoho@gmail.com>
2024-10-19 08:05:34 +0400

    Add Footer Component.

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 sm:flex-row">
+        <Link href="/">
+          <Image
+              src="/assets/images/logo.svg"
+              alt="logo"
+              width={128}
+              height={38}
+            />
+        </Link>
+
+        <p>© 2023 Evently. All Rights Reserved</p>
+      </div>
+    </footer>
   )
 }
 
