eb056d55c6b3ad9a7bccfe64329a9528ef8f00dc
notAnii <animazingtoho@gmail.com>
2024-10-30 01:10:19 +0400

    Implement orders and stripe payments.

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
 import {
   getEventById,
@@ -54,7 +55,8 @@ const EventDetails = async ({
                 </p>
               </div>
             </div>
-            {/* Checkout button */}
+
+            <CheckoutButton event={event} />
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
